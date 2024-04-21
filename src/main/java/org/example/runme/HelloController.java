da1368be04a0c88b56b4467db734be9739fae9d4
Tobias Blautenberg <tobiasglerupnielsen1994@gmail.com>
2024-04-21 12:57:19 +0200

    Made it a calculator.

--- src/main/java/org/example/runme/HelloController.java
+++ src/main/java/org/example/runme/HelloController.java
@@ -1,19 +1,63 @@
 package org.example.runme;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label display;
+
+    private String currentInput = "";
+    private double result = 0;
+    private char lastOperator = '=';
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void appendNumber(javafx.event.ActionEvent event) {
+        Button btn = (Button) event.getSource();
+        currentInput += btn.getText(); // Use getText() instead of getUserData()
+        display.setText(currentInput);
     }
 
     @FXML
-    protected void onHelloButtonClick2() {
-        welcomeText.setText("Goodbye!!!");
+    protected void appendOperator(javafx.event.ActionEvent event) {
+        Button btn = (Button) event.getSource();
+        char operator = btn.getText().charAt(0); // Use getText() instead of getUserData()
+        calculate();
+        lastOperator = operator;
+    }
+
+    @FXML
+    protected void calculate() {
+        if (!currentInput.isEmpty()) {
+            double number = Double.parseDouble(currentInput);
+            switch (lastOperator) {
+                case '+':
+                    result += number;
+                    break;
+                case '-':
+                    result -= number;
+                    break;
+                case '*':
+                    result *= number;
+                    break;
+                case '/':
+                    result /= number;
+                    break;
+                case '=':
+                    result = number;
+                    break;
+            }
+            display.setText(String.valueOf(result));
+            currentInput = "";
+        }
+    }
+
+    @FXML
+    protected void clear() {
+        display.setText("");
+        currentInput = "";
+        result = 0;
+        lastOperator = '=';
     }
 }
